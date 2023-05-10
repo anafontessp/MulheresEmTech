a2e60451e907164b0507437cd23cd43020cf029c
anafontessp <aosfontes@hotmail.com>
2023-05-10 15:59:01 -0300

    Trilhas

--- trilhas.php
+++ trilhas.php
@@ -1,5 +1,70 @@
 <?php include('cabecalho.php'); ?>
 
+center><h4>Transição profissional para a tecnologia. Primeiros passos para Planejar sua Mudança de Carreira.</h4></center>
+<br>
 
+
+<section>
+<center><iframe width="591" height="360" src="https://youtu.be/pD0bIsqvzlM" title="Mulheres na Computação | Camila Achutti | TEDxUSP" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
+</section>
+<br>
+<br>
+
+
+<section>
+
+<center><h4>Abaixo comece a estudar com as Trilhas_Tech.</h4></center>
+<br>
+</section>
+
+<div>
+		
+<div id="div1">
+  <div class="card" style="width:250px">
+    <img class="card-img-top"src="images/Logo.png" alt="">
+    <div class="card-body">
+      <h4 class="card-title">Trilhas_Tech</h4>
+      <p class="card-text">Como iniciar seus estudos em Tecnologia?</p>
+      <p><a href="https://encurtador.com.br/azOUY" target="_blank">Conheça mais</a></p>
+    </div>
+  </div>
+  <br>
+
+  <div class="div2">
+  <div class="card" style="width:250px">
+    <img class="card-img-top"src="images/Hedy2.jfif" alt="">
+    <div class="card-body">
+      <h4 class="card-title">Lógica de Programação</h4>
+      <p class="card-text">Conheça a base para ser um bom programador(a) e iniciar seu conhecimento em tecnologia. </p>
+      <p><a href="https://encurtador.com.br/myOQ0" target="_blank">Conheça mais</a></p>
+    </div>
+    <br>
+
+  </div>
+  <div class="div3">
+  <div class="card" style="width:250px">
+    <img class="card-img-top"src="images/grace5.jpg" alt="">
+    <div class="card-body">
+      <h4 class="card-title">Grace Hopper (1906-1992)</h4>
+      <p class="card-text">Considerada uma das pioneiras da programação de computadores e criadora da linguagem de programação COBOL.</p>
+      <p><a href="https://encurtador.com.br/rYZ25" target="_blank">Conheça mais</a></p>
+    </div>
+  </div>
+  <br>
+
+  </div>
+  <div class="div4">
+  <div class="card" style="width:250px">
+    <img class="card-img-top"src="images/eniac.jfif" alt="">
+    <div class="card-body">
+      <h4 class="card-title">Jean Jennings Bartik (1924-2011)</h4>
+      <p class="card-text">Uma das primeiras programadoras do ENIAC (Electronic Numerical Integrator and Computer), um dos primeiros computadores eletrônicos.</p>
+      <p><a href="https://www.ufrgs.br/enigma/as-mulheres-do-eniac/" target="_blank">Conheça mais</a></p>
+    </div>
+  </div>
+  <br>
+
+ 
+</div>
 
 <?php include('rodape.php'); ?>
