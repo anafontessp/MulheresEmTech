597cb7c6fd4f2fc2b4fbb1cfe719cc5e778c2541
albberrrt <albberrrt.yayay@gmail.com>
2023-05-17 14:04:41 -0300

    Cards centralizados

--- historia.php
+++ historia.php
@@ -1,113 +1,108 @@
 <?php include('cabecalho.php'); ?>
 
 <center><h4>Desigualdade de gênero no setor da tecnologia. Quais os desafios?</h4></center>
-<br>
 
 
 <section>
 <center><iframe width="591" height="360" src="https://www.youtube.com/embed/Qq9h2vVBZmQ" title="Mulheres na Computação | Camila Achutti | TEDxUSP" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
 </section>
-<br>
-<br>
-
-
+<br><br>
 <section>
-
-<center><h4>Inspire-se com mulheres que fazem parte da história da tecnologia.</h4></center>
-<br>
+  <center><h4>Inspire-se com mulheres que fazem parte da história da tecnologia.</h4></center>
 </section>
+<br>
 
-<section>
-		
-<div class="container mt-3">
-  <div class="card" style="width:250px">
-    <img class="card-img-top"src="images/ada3.jpg" alt="">
-    <div class="card-body">
-      <h4 class="card-title">Ada Lovelace (1815-1852)</h4>
-      <p class="card-text">Considerada a primeira programadora da história por suas contribuições ao trabalho de Charles Babbage na criação da "Máquina Analítica".</p>
-      <p><a href="https://encurtador.com.br/azOUY" target="_blank">Conheça mais</a></p>
-    </div>
-  </div>
-  <br>
+<section class="main-section">
+  <div class="main-02">
 
-  <div class="container mt-3">
-  <div class="card" style="width:250px">
-    <img class="card-img-top"src="images/Hedy2.jfif" alt="">
-    <div class="card-body">
-      <h4 class="card-title">Hedy Lamar (1914-2000)</h4>
-      <p class="card-text">Atriz e inventora austríaca-americana que criou um sistema de comunicação secreto para ajudar os Aliados na Segunda Guerra Mundial, que se tornou a base para a tecnologia Wi-Fi, Bluetooth e GPS.</p>
-      <p><a href="https://encurtador.com.br/myOQ0" target="_blank">Conheça mais</a></p>
-    </div>
-    <br>
-
-  </div>
-  <div class="container mt-3">
-  <div class="card" style="width:250px">
-    <img class="card-img-top"src="images/grace5.jpg" alt="">
-    <div class="card-body">
-      <h4 class="card-title">Grace Hopper (1906-1992)</h4>
-      <p class="card-text">Considerada uma das pioneiras da programação de computadores e criadora da linguagem de programação COBOL.</p>
-      <p><a href="https://encurtador.com.br/rYZ25" target="_blank">Conheça mais</a></p>
-    </div>
-  </div>
-  <br>
-
-  </div>
-  <div class="container mt-3">
-  <div class="card" style="width:250px">
-    <img class="card-img-top"src="images/eniac.jfif" alt="">
-    <div class="card-body">
-      <h4 class="card-title">Jean Jennings Bartik (1924-2011)</h4>
-      <p class="card-text">Uma das primeiras programadoras do ENIAC (Electronic Numerical Integrator and Computer), um dos primeiros computadores eletrônicos.</p>
-      <p><a href="https://www.ufrgs.br/enigma/as-mulheres-do-eniac/" target="_blank">Conheça mais</a></p>
-    </div>
-  </div>
-  <br>
-
-  </div>
-  <div class="containermt-3">
-  <div class="card" style="width:250px">
-    <img class="card-img-top"src="images/sister.jfif" alt="">
-    <div class="card-body">
-      <h4 class="card-title">Sister Mary Kenneth Keller (1913-1985)</h4>
-      <p class="card-text">Foi a primeira mulher a receber um Ph.D. em ciência da computação nos Estados Unidos e ajudou a desenvolver a linguagem de programação BASIC.</p>
-      <p><a href="https://arquivo.canaltech.com.br/internet/mulheres-historicas-irma-mary-kenneth-keller-pioneira-na-ciencia-da-computacao-74111/" target="_blank">Conheça mais</a></p>
-   
-    </div>
-  </div>
-
-  <div class="container mt-3">
-  <div class="card" style="width:250px">
-    <img class="card-img-top"src="images/radia.jfif" alt="">
-    <div class="card-body">
-      <h4 class="card-title">Radia Perlman (1951)</h4>
-      <p class="card-text"> Conhecida como "a mãe da Internet" por suas contribuições para o desenvolvimento de protocolos de rede e para o algoritmo de roteamento Spanning Tree.</p>
-      <p><a href="https://engenharia360.com/mulheres-ciencia-radia-perlman/" target="_blank">Conheça mais</a></p>
-    </div>
+    <div class="container card-container mt-3">
+      <div class="card card-historia" style="width:250px">
+        <img class="card-img-top"src="images/ada3.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">Ada Lovelace (1815-1852)</h4>
+          <p class="card-text">Considerada a primeira programadora da história por suas contribuições ao trabalho de Charles Babbage na criação da "Máquina Analítica".</p>
+          <p class="card-saibamais"><a href="https://encurtador.com.br/azOUY" target="_blank">Conheça mais</a></p>
+        </div>
+      </div>
     </div>
 
-  </div>
-  <div class="container mt-3">
-  <div class="card" style="width:250px">
-    <img class="card-img-top"src="images/susan.jpg" alt="">
-    <div class="card-body">
-      <h4 class="card-title">Susan Kare (1954)</h4>
-      <p class="card-text"> Designer gráfica que criou muitos dos ícones e fontes originais para o sistema operacional Macintosh da Apple.</p>
-      <p><a href="https://arquivo.canaltech.com.br/internet/mulheres-historicas-susan-kare-revolucionou-o-desgin-grafico-nos-computadores-76629/" target="_blank">Conheça mais</a></p>
-    </div>
-  </div>
+      <div class="container card-container mt-3">
+        <div class="card card-historia" style="width:250px">
+          <img class="card-img-top"src="images/Hedy2.jfif" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Hedy Lamar (1914-2000)</h4>
+            <p class="card-text">Atriz e inventora austríaca-americana que criou um sistema de comunicação secreto para ajudar os Aliados na Segunda Guerra Mundial, que se tornou a base para a tecnologia Wi-Fi, Bluetooth e GPS.</p>
+            <p class="card-saibamais"><a href="https://encurtador.com.br/myOQ0" target="_blank">Conheça mais</a></p>
+          </div>
+        </div>
+      </div>
+      
+      <div class="container card-container mt-3">
+        <div class="card card-historia" style="width:250px">
+          <img class="card-img-top"src="images/grace5.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Grace Hopper (1906-1992)</h4>
+            <p class="card-text">Considerada uma das pioneiras da programação de computadores e criadora da linguagem de programação COBOL.</p>
+            <p class="card-saibamais"><a href="https://encurtador.com.br/rYZ25" target="_blank">Conheça mais</a></p>
+          </div>
+        </div>
+      </div>
 
-  </div>
-  <div class="container mt-3">
-  <div class="card" style="width:250px">
-    <img class="card-img-top"src="images/shafi.jpg" alt="">
-    <div class="card-body">
-      <h4 class="card-title">Shafi Goldwasser (1958)</h4>
-      <p class="card-text"> Contribuiu para a criptografia de chave pública e é uma das poucas mulheres a receber uma Medalha Fields, o prêmio mais prestigioso da matemática.</p>
-      <p><a href="https://wiki.inf.ufpr.br/computacao/doku.php?id=s:shafrira_goldwasser" target="_blank">Conheça mais</a></p>
-    </div>
-  </div>
+      <div class="container card-container mt-3">
+        <div class="card card-historia" style="width:250px">
+          <img class="card-img-top"src="images/eniac.jfif" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Jean Jennings Bartik (1924-2011)</h4>
+            <p class="card-text">Uma das primeiras programadoras do ENIAC (Electronic Numerical Integrator and Computer), um dos primeiros computadores eletrônicos.</p>
+            <p class="card-saibamais"><a href="https://www.ufrgs.br/enigma/as-mulheres-do-eniac/" target="_blank">Conheça mais</a></p>
+          </div>
+        </div>
+      </div>
 
+      <div class="container card-container mt-3">
+        <div class="card card-historia" style="width:250px">
+          <img class="card-img-top"src="images/sister.jfif" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Sister Mary Kenneth Keller (1913-1985)</h4>
+            <p class="card-text">Foi a primeira mulher a receber um Ph.D. em ciência da computação nos Estados Unidos e ajudou a desenvolver a linguagem de programação BASIC.</p>
+            <p class="card-saibamais"><a href="https://arquivo.canaltech.com.br/internet/mulheres-historicas-irma-mary-kenneth-keller-pioneira-na-ciencia-da-computacao-74111/" target="_blank">Conheça mais</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="container card-container mt-3">
+        <div class="card card-historia" style="width:250px">
+          <img class="card-img-top"src="images/radia.jfif" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Radia Perlman (1951)</h4>
+            <p class="card-text"> Conhecida como "a mãe da Internet" por suas contribuições para o desenvolvimento de protocolos de rede e para o algoritmo de roteamento Spanning Tree.</p>
+            <p class="card-saibamais"><a href="https://engenharia360.com/mulheres-ciencia-radia-perlman/" target="_blank">Conheça mais</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="container card-container mt-3">
+        <div class="card card-historia" style="width:250px">
+          <img class="card-img-top"src="images/susan.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Susan Kare (1954)</h4>
+            <p class="card-text"> Designer gráfica que criou muitos dos ícones e fontes originais para o sistema operacional Macintosh da Apple.</p>
+            <p class="card-saibamais"><a href="https://arquivo.canaltech.com.br/internet/mulheres-historicas-susan-kare-revolucionou-o-desgin-grafico-nos-computadores-76629/" target="_blank">Conheça mais</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="container card-container mt-3">
+        <div class="card card-historia" style="width:250px">
+          <img class="card-img-top"src="images/shafi.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Shafi Goldwasser (1958)</h4>
+            <p class="card-text"> Contribuiu para a criptografia de chave pública e é uma das poucas mulheres a receber uma Medalha Fields, o prêmio mais prestigioso da matemática.</p>
+            <p class="card-saibamais"><a href="https://wiki.inf.ufpr.br/computacao/doku.php?id=s:shafrira_goldwasser" target="_blank">Conheça mais</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 
 
--- trilhas.php
+++ trilhas.php
@@ -16,35 +16,66 @@
 <center><h4>Abaixo comece a estudar com as Trilhas_Tech.</h4></center>
 <br>
 </section>
-	
-<body>
 
-
-    <div class="container">
+<section class="main-section">
+  <div class="main-02">
+      
+    <div class="container card-container mt-3">
       <div class="card" style="width:250px">
-        <h2>Transição</h2>
-        <p>Transição de Carreira para Tecnologia.</p>
-        <a href="#">Saiba mais</a>
-        <img class="image-card" src="images/Logo.png" alt="" />
-      </div>
-
-      <div class="card" style="width:250px">
-        <h2>Lógica</h2>
-        <p>Aprenda Lógica de Programação.</p>
-        <a href="#">Saiba mais</a>
-        <img class="image-card" src="images/Logo.png" alt="" />
-      </div>
-
-      <div class="card red" style="width:250px">
-        <h2>Profissões</h2>
-        <p>Como ser um programador(a)?</p>
-        <a href="#">Saiba mais</a>
-        <img class="image-card" src="images/Logo.png" alt="" />
+        <img class="card-img-top"src="images/Logo.png" alt="">
+        <div class="card-body">
+          <h4 class="card-title">Trilhas_Tech</h4>
+          <p class="card-text">Como iniciar seus estudos em Tecnologia?</p>
+          <p class="card-saibamais"><a href="https://encurtador.com.br/azOUY" target="_blank">Conheça mais</a></p>
+        </div>
       </div>
     </div>
-</div>
 
-</body>
+    <div class="container card-container mt-3">
+      <div class="card" style="width:250px">
+        <img class="card-img-top"src="images/Logo.png" alt="">
+        <div class="card-body">
+          <h4 class="card-title">Lógica de Programação</h4>
+          <p class="card-text">Conheça a base para ser um bom programador(a) e iniciar seu conhecimento em tecnologia. </p>
+          <p class="card-saibamais"><a href="https://encurtador.com.br/myOQ0" target="_blank">Conheça mais</a></p>
+        </div>
+      </div>
+    </div>
 
+    <div class="container card-container mt-3">
+      <div class="card" style="width:250px">
+        <img class="card-img-top"src="images/Logo.png" alt="">
+        <div class="card-body">
+          <h4 class="card-title">Dicas</h4>
+          <p class="card-text">Considerada uma das pioneiras da programação de computadores e criadora da linguagem de programação COBOL.</p>
+          <p class="card-saibamais"><a href="https://encurtador.com.br/rYZ25" target="_blank">Conheça mais</a></p>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container card-container mt-3">
+      <div class="card" style="width:250px">
+        <img class="card-img-top"src="images/Logo.png" alt="">
+        <div class="card-body">
+          <h4 class="card-title">Card teste</h4>
+          <p class="card-text">Card criado somente para testes. Mudar nome e descrição do card assim que possível.</p>
+          <p class="card-saibamais"><a href="https://encurtador.com.br/rYZ25" target="_blank">Conheça mais</a></p>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container card-container mt-3">
+      <div class="card" style="width:250px">
+        <img class="card-img-top"src="images/Logo.png" alt="">
+        <div class="card-body">
+          <h4 class="card-title">Card teste</h4>
+          <p class="card-text">Card criado somente para testes. Mudar nome e descrição do card assim que possível.</p>
+          <p class="card-saibamais"><a href="https://encurtador.com.br/rYZ25" target="_blank">Conheça mais</a></p>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</section>
 
 <?php include('rodape.php'); ?>
